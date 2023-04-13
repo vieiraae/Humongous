f1f0a042aa5b568c76c174a97833ea159658a68e
Alexandre Vieira <alexandre.vieira@microsoft.com>
2023-04-13 21:01:37 +0100

    github action

--- Humongous.Healthcare.Web/ClientApp/src/index.js
+++ Humongous.Healthcare.Web/ClientApp/src/index.js
@@ -13,7 +13,7 @@ ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
     <App />
   </BrowserRouter>,
-  rootElement);
+  rootElement); 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
